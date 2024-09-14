84509523d993fbadfb60066f12436ab2c60bf83d
Sebastian Casas Holguin <sebastian_XxebaxX@outlook.com>
2024-09-13 21:56:47 -0500

    V1_Agregar el Enunciado, y Declarar Variables.

---
+++ Punto2_Ecuacion/Punto2_Ecuacion/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Escriba un programa que pida los coeficientes de una ecuación de primer grado (a * x + b = 0) y muestre la solución.
+        //Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los números sean solución. La fórmula de las soluciones es x = -b/a
+
+        //Definimos las variables
+
+        double a, b, x;
+
+        }
+    }
+}
